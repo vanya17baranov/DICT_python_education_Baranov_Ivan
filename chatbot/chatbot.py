ed614a177375fd8820cc0ecbd1793762170ca755
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-07 17:54:38 +0200

    Chatbot 2-nd stage

---
+++ chatbot/chatbot.py
@@ -0,0 +1,7 @@
+name = "VanyaChatbot"
+birth_date = 2021
+print(f"Hello! My name is {name}")
+print(f"I was created in {birth_date}")
+print("Please, remind me your name")
+your_name = input()
+print(f"What a great name you have {your_name}")
