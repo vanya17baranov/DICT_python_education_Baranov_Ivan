1baeaca51da061f0bb7c51fd6a2d78959290cf69
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-08 19:37:02 +0200

    corrected hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,7 +2,7 @@ print("HANGMAN")
 print("The game will be available soon")
 print("Guess the word:")
 answer = input()
-if answer == python:
+if answer == "python":
     print("You survived!")
 else:
     print("You lost!")
