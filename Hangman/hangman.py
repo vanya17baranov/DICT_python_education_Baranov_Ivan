598ddf0e7db13ac316e660ad28ca5573acea9f22
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-13 02:26:18 +0200

    hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,12 +1,28 @@
 import random
 print("HANGMAN")
 print("The game will be available soon")
+attempts = 8
 words = ["java", "javascript", "python", "php"]
 answers = random.choice(words)
-help = answers[0] + answers[1] + answers[2] + "-" * (len(answers) -3)
-print("Guess the word " + help + ":")
-answer = input()
-if answer == answers:
-    print("You survived!")
-else:
-    print("You lost!")
+guesses = []
+done = False
+while not done:
+    for letter in answers:
+        if letter in guesses:
+            print(letter, end="")
+        else:
+            print("_", end="")
+    print("")
+    guess = input("Input a letter:")
+    guesses.append(guess)
+    attempts -= 1
+    if guess not in answers:
+        print("That letter doesn\'t appear in the word")
+        if attempts == 0:
+            break
+    done = True
+    for letter in answers:
+        if letter not in guesses:
+            done = False
+print("Thanks for playing!")
+print("We\'ll see how well you did in the next stage")
