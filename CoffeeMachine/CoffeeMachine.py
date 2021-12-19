61c1470c541953cb52a6b27fbeec821e467bb971
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-19 23:37:53 +0200

    The 2-nd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,3 +1,10 @@
+amount = int(input("Write how many cups of coffee you will need:"))
+print(f"""
+For {amount} cups of coffee you will need:
+{amount * 200} ml of water,
+{amount * 50} ml of milk,
+{amount * 15} g of coffee beans
+""")
 print("Starting to make a coffee")
 print("Grinding coffee beans")
 print("Boiling water")
