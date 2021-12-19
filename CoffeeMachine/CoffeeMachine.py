d20777810d82f72a2247d599e93b8836009833ea
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-20 00:33:13 +0200

    The 4-th stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,20 +1,96 @@
-water_amount = int(input("Write how many ml of water the coffee machine has:"))
-milk_amount = int(input("Write how many ml of milk the coffee machine has:"))
-beans_amount = int(input("Write how many grams of coffee beans the coffee machine has:"))
-amount = int(input("Write how many cups of coffee you will need:"))
-storage = [water_amount // 200, milk_amount // 50, beans_amount // 15]
-if min(storage) > amount:
-    print(f"Yes, I can make that amount of coffee (and even {min(storage) - amount} more than that)")
-elif min(storage) == amount:
-    print("Yes, I can make that amount of coffee")
-elif min(storage) < amount:
-    print(f"No, I can make only {min(storage)} cups of coffee")
-print("""
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
-""")
+class CoffeeMachine:
+
+    def __init__(self):
+        self.water_amount = 400
+        self.milk_amount = 540
+        self.beans_amount = 120
+        self.money_amount = 550
+        self.cups_amount = 9
+
+    def amount(self):
+        print(f"""The coffee machine has:
+        {self.water_amount} of water
+        {self.milk_amount} of milk
+        {self.beans_amount} of coffee beans
+        {self.cups_amount} of disposable cups
+        {self.money_amount} of money""")
+
+    def money(self):
+        print(f"I gave you {self.money_amount}")
+        self.money_amount = 0
+        print("")
+        print(f"""The coffee machine has:
+              {self.water_amount} of water
+              {self.milk_amount} of milk
+              {self.beans_amount} of coffee beans
+              {self.cups_amount} of disposable cups
+              {self.money_amount} of money""")
+
+    def add(self):
+        self.water_amount = self.water_amount + int(input("Write how many ml of water you want to add:"))
+        self.milk_amount = self.milk_amount + int(input("Write how many ml of milk you want to add:"))
+        self.beans_amount = self.beans_amount + int(input("Write how many grams of coffee beans you want to add:"))
+        self.cups_amount = self.cups_amount + int(input("Write how many disposable coffee cups you want to add:"))
+        print(f"""The coffee machine has:
+              {self.water_amount} of water
+              {self.milk_amount} of milk
+              {self.beans_amount} of coffee beans
+              {self.cups_amount} of disposable cups
+              {self.money_amount} of money""")
+
+    def coffee(self):
+        coffee_types = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        if coffee_types == "1":
+            self.water_amount -= 250
+            self.beans_amount -= 16
+            self.money_amount += 4
+            self.cups_amount -= 1
+            print("Making your coffee")
+            print(f"""The coffee machine has:
+              {self.water_amount} of water
+              {self.milk_amount} of milk
+              {self.beans_amount} of coffee beans
+              {self.cups_amount} of disposable cups
+              {self.money_amount} of money""")
+        elif coffee_types == '2':
+            self.water_amount -= 350
+            self.milk_amount -= 75
+            self.beans_amount -= 20
+            self.money_amount += 7
+            self.cups_amount -= 1
+            print("Making your coffee")
+            print(f"""The coffee machine has:
+              {self.water_amount} of water
+              {self.milk_amount} of milk
+              {self.beans_amount} of coffee beans
+              {self.cups_amount} of disposable cups
+              {self.money_amount} of money""")
+        elif coffee_types == "3":
+            self.water_amount -= 200
+            self.milk_amount -= 100
+            self.beans_amount -= 12
+            self.money_amount += 6
+            self.cups_amount -= 1
+            print("Making your coffee")
+            print(f"""The coffee machine has:
+              {self.water_amount} of water
+              {self.milk_amount} of milk
+              {self.beans_amount} of coffee beans
+              {self.cups_amount} of disposable cups
+              {self.money_amount} of money""")
+
+
+defs = CoffeeMachine()
+
+
+def menu():
+    choose = input('Write action (buy, fill, take):')
+    if choose == 'buy':
+        defs.coffee()
+    elif choose == 'take':
+        defs.money()
+    elif choose == 'fill':
+        defs.add()
+
+
+menu()
