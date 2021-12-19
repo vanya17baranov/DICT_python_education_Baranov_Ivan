d69c33d8c6506707a952b8cf3d2814b11b3933e5
Vanya Baranov <i.i.baranov@student.khai.edu>
2021-12-19 18:25:59 +0200

    hangman 7-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,34 +1,45 @@
 import random
 print("HANGMAN")
-print("The game will be available soon")
-attempts = 8
-words = ["java", "javascript", "python", "php"]
-answers = random.choice(words)
-guesses = []
-done = False
-while not done:
-    for letter in answers:
-        if letter in guesses:
-            print(letter, end="")
+def main():
+    attempts = 8
+    words = ["java", "javascript", "python", "php"]
+    answers = random.choice(words)
+    guesses = []
+    done = False
+    incorrect_guesses = []
+    while not done:
+        for letter in answers:
+            if letter in guesses:
+                print(letter, end='')
+            else:
+                print("_", end="")
+        print("")
+        guess = input("Input a letter:")
+        if len(guess) != 1:
+            print("You should input a single letter.")
+        elif guess.isupper() is True:
+            print("Please enter a lowercase English letter.")
+        elif guess in incorrect_guesses:
+            print("You\'ve already guessed this letter.")
+        elif guess not in answers:
+            attempts -= 1
+            incorrect_guesses.append(guess)
+            print("That letter doesn\'t appear in the word")
+        elif guess in guesses:
+            print("You\'ve already guessed this letter.")
         else:
-            print("_", end="")
-    print("")
-    guess = input("Input a letter:")
-    if guess in guesses:
-        attempts -= 1
-        print('No improvements')
-    if guess in answers:
-        guesses.append(guess)
-    else:
-        attempts -= 1
-    print("That letter doesn\'t appear in the word")
-    if attempts == 0:
-        break
-    done = True
-    for letter in answers:
-        if letter not in guesses:
-            done = False
-if done:
-    print('Congrats')
-else:
-    print('Game over')
+            guesses.append(guess)
+            if attempts == 0:
+                break
+            done = True
+            for letter in answers:
+                if letter not in guesses:
+                    print(1)
+                    done = False
+        if done:
+            print("You guessed the word {}!".format(answers))
+            print("You survived!")
+        else:
+            print("You lost!")
+            print(done)
+main()
